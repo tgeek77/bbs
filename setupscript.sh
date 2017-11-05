44faf9ad29efe82a5049557221d1b97722e23c05
J. S. Evans <jevans@suse.com>
2017-11-05 12:44:15 +0100

    Update setupscript.sh

--- setupscript.sh
+++ setupscript.sh
@@ -22,8 +22,8 @@ echo "deb http://deb.torproject.org/torproject.org xenial main" >> /etc/apt/sour
 gpg --keyserver keys.gnupg.net --recv A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89
 gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
 
-apt update && apt install tor deb.torproject.org-keyring
+apt update && apt install tor deb.torproject.org-keyring -y
 
 ## setup salt-minion
 echo "10.0.1.110  salt jserver" >> /etc/hosts
-apt install salt-minion
+apt install salt-minion -y
